e918d91d4505e6bd2c9cf8eace2f8b72b68a813a
flar2 <asegaert@gmail.com>
2015-12-05 21:12:48 -0500

    msm_performance: Make input boosting optional
    Signed-off-by: Sai Vishal <saivishal2001@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -27,6 +27,8 @@
 #include <linux/kthread.h>
 
 static unsigned int use_input_evts_with_hi_slvt_detect;
+static int touchboost = 1;
+
 static struct mutex managed_cpus_lock;
 
 static int touchboost = 0;
@@ -406,6 +408,11 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (touchboost == 0)
 		return 0;
 
+	const char *reset = "0:0 4:0";
+
+	if (touchboost == 0)
+		cp = reset;
+
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -413,7 +420,11 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+		cp = reset;
+	else
+		cp = buf;
+
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
