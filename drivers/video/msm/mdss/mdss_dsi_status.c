4d10cdd63157aff6c8cc147f35263edd8e802915
Alex Naidis <alex.naidis@linux.com>
2017-05-05 18:38:05 +0200

    msm: mdss: Use a high priority WQ for irq_done work
    Since this work is latency critical it needs to
    use the shared high priority workqueue.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_status.c
+++ drivers/video/msm/mdss/mdss_dsi_status.c
@@ -96,6 +96,7 @@ irqreturn_t hw_vsync_handler(int irq, void *data)
 
 	if (!atomic_read(&ctrl_pdata->te_irq_ready))
 		atomic_inc(&ctrl_pdata->te_irq_ready);
+	queue_work(system_highpri_wq, &pstatus_data->irq_done);
 
 	return IRQ_HANDLED;
 }
